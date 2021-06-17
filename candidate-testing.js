4e6f54ab4cfc2ed83860e2264c54fe476903fbff
CharlieD4011 <84350098+CharlieDavidson4011@users.noreply.github.com>
2021-06-17 18:23:32 +0000

    Solidified Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,37 +8,44 @@ let candidateName = ""
 let question = "Input Candidate Name:"
 let correctAnswer = ""
 let candidateAnswer = ""
-let questions = ["Who was the first female American astronaut in space?", "True or false: 5 kilometers === 5000 meters?", "(5+3)/2*10=?", "Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2?", "What is the minimum crew size for the ISS?"]
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometers === 5000 meters?", "(5+3)/2*10=?", "Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2?", "What is the minimum crew size for the ISS?"]
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"]
-let candidateAnswers = ""
-
+let candidateAnswers = []; // use this array to store candidate responses to questions.
+let numQuestions = questions.length
+let numCorrectAnswers = correctAnswers.length
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-input.question(question)
-candidateAnswer = ""
+input.question("Candidate Name:")
+
 }
 
 function askQuestion() {
+ // candidateAnswers.push(input.question(questions))//this quite correct here....
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-input.question("1)" + questions[0])
-console.log("Your Answer:" + candidateAnswers[0])
+
+
+//yes questions[0] is defined; currently you are not putting the user response somewhere
+//maybe something like....very close to that almost there....
+candidateAnswers.push(input.question("1)" + questions[0]));
+//input.question("1)" + questions[0]) //here it looks like you are asking the question, but now saving the answer somewhere
+console.log("Your Answer:" + (candidateAnswers[0])) //this is what is giving you that function in your answer in the console
 console.log("Correct Answer:" + correctAnswers[0])
 console.log("")
-input.question("2)" + questions[1])
-console.log("Your Answer:" + candidateAnswers)
+candidateAnswers.push(input.question("2)" + questions[1]));//so you will have to do something similar to this area that we did on 30?
+console.log("Your Answer:" + candidateAnswers[1])
 console.log("Correct Answer:" + correctAnswers[1])
 console.log("")
-input.question("3)" + questions[2])
-console.log("Your Answer:" + candidateAnswers)
+candidateAnswers.push(input.question("3)" + questions[2]));
+console.log("Your Answer:" + candidateAnswers[2])
 console.log("Correct Answer:" + correctAnswers[2])
 console.log("")
-input.question("3)" + questions[3])
-console.log("Your Answer:" + candidateAnswers)
+candidateAnswers.push(input.question("3)" + questions[3]));
+console.log("Your Answer:" + candidateAnswers[3])
 console.log("Correct Answer:" + correctAnswers[3])
 console.log("")
-input.question("4)" + questions[4])
-console.log("Your Answer:" + candidateAnswers)
+candidateAnswers.push(input.question("4)" + questions[4]));
+console.log("Your Answer:" + candidateAnswers[4])
 console.log("Correct Answer:" + correctAnswers[4])
 
 }
@@ -48,9 +55,10 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  let grade = 100% 
-
-  
+  let grade = 20% 
+   (candidateAnswers === correctAnswers) 
+  console.log(grade)
+   
 
   return grade;
 }
