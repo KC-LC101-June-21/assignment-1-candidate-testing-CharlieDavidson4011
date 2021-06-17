e8d41566a40553f83f7f863d5e28a0af20d221da
CharlieD4011 <84350098+CharlieDavidson4011@users.noreply.github.com>
2021-06-17 11:31:48 +0000

    Completed parts 1 and most of 2.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,24 +3,43 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = ""
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "Input Candidate Name:"
+let correctAnswer = ""
+let candidateAnswer = ""
+let questions = ["Who was the first female American astronaut in space?", "True or false: 5 kilometers === 5000 meters?", "(5+3)/2*10=?", "Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2?", "What is the minimum crew size for the ISS?"]
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"]
+let candidateAnswers = ""
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+input.question(question)
+candidateAnswer = ""
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+input.question("1)" + questions[0])
+console.log("Your Answer:" + candidateAnswers[0])
+console.log("Correct Answer:" + correctAnswers[0])
+console.log("")
+input.question("2)" + questions[1])
+console.log("Your Answer:" + candidateAnswers)
+console.log("Correct Answer:" + correctAnswers[1])
+console.log("")
+input.question("3)" + questions[2])
+console.log("Your Answer:" + candidateAnswers)
+console.log("Correct Answer:" + correctAnswers[2])
+console.log("")
+input.question("3)" + questions[3])
+console.log("Your Answer:" + candidateAnswers)
+console.log("Correct Answer:" + correctAnswers[3])
+console.log("")
+input.question("4)" + questions[4])
+console.log("Your Answer:" + candidateAnswers)
+console.log("Correct Answer:" + correctAnswers[4])
 
 }
 
@@ -29,7 +48,8 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  let grade;
+  let grade = 100% 
+
   
 
   return grade;
