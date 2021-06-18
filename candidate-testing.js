d426e6c90b9b44ab70e6c3f49ea4e79833070457
CharlieD4011 <84350098+CharlieDavidson4011@users.noreply.github.com>
2021-06-18 04:51:44 +0000

    Final Assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,11 +8,10 @@ let candidateName = ""
 let question = "Input Candidate Name:"
 let correctAnswer = ""
 let candidateAnswer = ""
-let questions = ["Who was the first American woman in space?", "True or false: 5 kilometers === 5000 meters?", "(5+3)/2*10=?", "Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2?", "What is the minimum crew size for the ISS?"]
+let questions = ["1) Who was the first American woman in space? ", "2) True or false: 5 kilometers === 5000 meters. ", "3) (5+3)/2*10= ? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "]
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"]
-let candidateAnswers = []; // use this array to store candidate responses to questions.
-let numQuestions = questions.length
-let numCorrectAnswers = correctAnswers.length
+let candidateAnswers = []; 
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -24,52 +23,72 @@ function askQuestion() {
  // candidateAnswers.push(input.question(questions))//this quite correct here....
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
+for (let i = 0; i < questions.length; i ++){
+	candidateAnswers.push(input.question(questions[i])); 
+   console.log(`Your Answer: ${candidateAnswers[i]}
+Correct Answer: ${correctAnswers[i]}`)
+  
+}
+//console.log(">>> Overall Grade 
 
-candidateAnswers.push(input.question("1)" + questions[0]));
-
-console.log("Your Answer:" + (candidateAnswers[0])) 
-console.log("Correct Answer:" + correctAnswers[0])
-console.log("")
-candidateAnswers.push(input.question("2)" + questions[1]));
-console.log("Your Answer:" + candidateAnswers[1])
-console.log("Correct Answer:" + correctAnswers[1])
-console.log("")
-candidateAnswers.push(input.question("3)" + questions[2]));
-console.log("Your Answer:" + candidateAnswers[2])
-console.log("Correct Answer:" + correctAnswers[2])
-console.log("")
-candidateAnswers.push(input.question("3)" + questions[3]));
-console.log("Your Answer:" + candidateAnswers[3])
-console.log("Correct Answer:" + correctAnswers[3])
-console.log("")
-candidateAnswers.push(input.question("4)" + questions[4]));
-console.log("Your Answer:" + candidateAnswers[4])
-console.log("Correct Answer:" + correctAnswers[4])
 
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 let correctCandidateAnswers = 0; 
-let overallGrade = (correctCandidateAnswers.length/questions.length)*100
-for (let i = 0; i < candidateAnswers.length; i ++) {
-  if (candidateAnswers[i] === correctAnswers[i]) {
-    correctCandidateAnswers = overallGrade
-  
 
-  console.log(correctCandidateAnswers)
+for (let i = 0; i < candidateAnswers.length; i++) {
 
-  
-  return grade = 0;
+  if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+  correctCandidateAnswers++; 
+  }
 }
+
+let grade;
+  console.log((correctCandidateAnswers/questions.length)*100 + "%" );
+
+grade = (correctCandidateAnswers/questions.length)*100;
+// 
+
+if (grade < 80) {
+console.log(">>> Status: FAILED <<<")
 }
+if (grade >= 80 ) {
+  console.log(">>> Status: PASSED <<<")
+}
+
+
+
+
 } 
+
 function runProgram() {
+  name = "Candidate Name:  "
+  // candidateAnswer[i] currently refers to a value of candidateAnswer not a number
+ 
+
+
+
+// join the zoom through slack
+
+// ` essentially replaces " 
+// `${} references a variable`
+//The variable in this case would be candidateAnswer, correct?
+
+
+
+  
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   
+  
+  
+
+  
   askQuestion();
+//   for (let i = 0; i < questions.length; i++)
+// console.log(questions[i])
   gradeQuiz(this.candidateAnswers);
 }
 
